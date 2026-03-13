90c8688f8dcf55dbfa67fb6b546ce25cba65f090
Md Raja <mdraja.tech.03@gmail.com>
2026-03-13 20:57:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,81 @@
-let speech = new SpeechSynthesisUtterance();
+let speech = new SpeechSynthesisUtterance()
 
+let voices = []
 
-let voices = [];
+let voiceSelect = document.getElementById("voice")
 
-let voiceSelect = document.querySelector("select");
+let text = document.getElementById("text")
+
+let speed = document.getElementById("speed")
+
+let pitch = document.getElementById("pitch")
+
+let count = document.getElementById("count")
+
+let speakBtn = document.getElementById("speak")
+
+let stopBtn = document.getElementById("stop")
+
+let copyBtn = document.getElementById("copy")
+
+let clearBtn = document.getElementById("clear")
 
 window.speechSynthesis.onvoiceschanged = () => {
-    voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
 
-    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
-};
+voices = speechSynthesis.getVoices()
 
+voices.forEach((voice,i)=>{
 
-voiceSelect.addEventListener("change", () => {
-    speech.voice = voices[voiceSelect.value];
-});
+voiceSelect.options[i] = new Option(voice.name,i)
 
+})
 
+speech.voice = voices[0]
 
+}
 
-document.querySelector("button").addEventListener("click", () =>{
-    speech.text = document.querySelector("textarea").value;
-    window.speechSynthesis.speak(speech);
-});
+voiceSelect.addEventListener("change",()=>{
+
+speech.voice = voices[voiceSelect.value]
+
+})
+
+speakBtn.onclick = ()=>{
+
+speech.text = text.value
+
+speech.rate = speed.value
+
+speech.pitch = pitch.value
+
+speechSynthesis.speak(speech)
+
+}
+
+stopBtn.onclick = ()=>{
+
+speechSynthesis.cancel()
+
+}
+
+copyBtn.onclick = ()=>{
+
+navigator.clipboard.writeText(text.value)
+
+alert("Text Copied")
+
+}
+
+clearBtn.onclick = ()=>{
+
+text.value = ""
+
+count.innerText = 0
+
+}
+
+text.addEventListener("input",()=>{
+
+count.innerText = text.value.length
+
+})
